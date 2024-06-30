4b47871c7b54b88f4e1ae4e6d9f20f5ed30c9f86
TappCloud <119796698+TappCloud@users.noreply.github.com>
2024-06-30 12:33:42 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 4096,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*',
+    mediaroot: './media',
+    webroot: './www',
+    api: true
+  },
+  trans: {
+    ffmpeg: '/usr/bin/ffmpeg', // Path to ffmpeg on Domcloud.co
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: false
+      }
+    ]
+  }
+};
+
+const nms = new NodeMediaServer(config);
+nms.run();
+
+console.log('RTMP server running...');
